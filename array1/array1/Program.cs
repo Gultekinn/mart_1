52adc1703b8d196836880ad8ddd0538d98a9cd13
unknown <azizovagultakin@gmail.com>
2023-03-01 08:07:43 +0300

    first commit

---
+++ array1/array1/Program.cs
@@ -0,0 +1,27 @@
+﻿//1. Ədədlərdən ibarət arrayın elementleri cemini tapan alqoritm
+
+
+//int[] array = { 1, 5, 8, 12, 24, 29 };
+//int sum = 0;
+//for (int i = 0; i < array.Length; i++)
+//{
+//    sum += array[i];
+
+//    {
+//        Console.WriteLine(sum);
+//    }
+//}
+
+//2. Sozlerden ibaret arrayde uzunlugu 4-den boyuk olan sozleri ekrana çap edin
+int a=4;
+string[] students = { "gulu", "gunay", "lale", "nergiz", "ayde", "nezi" };
+for (int i = 0; i < students.Length; i++) 
+{
+    if (students[i].Length>4)
+    {
+
+        Console.WriteLine(students[i]);
+    }
+
+    
+}
